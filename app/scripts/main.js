98568fa4d2cbb94e2f07433827e85f23e896f6ac
patrickgrey <websemantic@yahoo.co.uk>
2015-02-03 09:26:27 +0000

    Both navigation panels working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,17 @@
 /* jshint devel:true */
 (function (win, doc) {
     'use strict';
+    
     if (!win.addEventListener) {
         return;
     }
-    var linkclass = 'ec-menu-toggle',
-        activeclass = 'active',
+    
+    var linkclassNav = 'ec-menu-toggle-nav',
+        linkclassSettings = 'ec-menu-toggle-settings',
+        activeclassNav = 'active-nav',
+        activeclassSettings = 'active-settings',
         enhanceclass = 'cutsthemustard',
-        ignoreLinkClass = 'pc-button',
+        
         toggleClassName = function (element, toggleClass) {
             var reg = new RegExp('(\\s|^)' + toggleClass + '(\\s|$)');
             if (!element.className.match(reg)) {
@@ -16,14 +20,20 @@
                 element.className = element.className.replace(reg, '');
             }
         },
+        
         navListener = function (ev) {
             ev = ev || win.event;
             var target = ev.target || ev.srcElement;
-            if (target.className.indexOf(linkclass) !== -1) {
-                if (target.className.indexOf(ignoreLinkClass) !== -1) {ev.preventDefault();}
-                toggleClassName(doc.body, activeclass);
+            if (target.className.indexOf(linkclassNav) !== -1) {
+                ev.preventDefault();
+                toggleClassName(doc.body, activeclassNav);
+            }
+            else if (target.className.indexOf(linkclassSettings) !== -1) {
+                ev.preventDefault();
+                toggleClassName(doc.body, activeclassSettings);
             }
         };
+        
     doc.documentElement.className += ' ' + enhanceclass;
     doc.addEventListener('click', navListener, false);
     
